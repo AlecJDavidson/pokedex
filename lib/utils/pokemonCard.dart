8670d55818e4bb4331adacd6007f1572a68392b5
adavidson-smt <alec.davidson@smashmytrash.com>
2021-06-04 11:19:59 -0400

    Added pictures

--- lib/utils/pokemonCard.dart
+++ lib/utils/pokemonCard.dart
@@ -21,6 +21,7 @@ class _PokemonObjectState extends State<PokemonObject> {
   int entry;
   String name;
   String type1;
+  String sprite;
 
   @override
   void initState() {
@@ -34,41 +35,46 @@ class _PokemonObjectState extends State<PokemonObject> {
       entry = pokemonData['data'][dexEntry]['entry'];
       name = pokemonData['data'][dexEntry]['name'];
       type1 = pokemonData['data'][dexEntry]['types'][0];
+      sprite = ('assets/sprites/'+entry.toString()+'.png');
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("images/image.png"),
-          child: SizedBox(
-            height: 300,
-            width: 300,
-            child: Card(
-              color: getColor(type1),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50.0)),
-              child: TextButton(
-                child: Text(
-                  '$name' + '  dex: $entry' + ' type: $type1',
-                ),
-                onPressed: () async {
-                  await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return AboutScreen();
-                      },
-                    ),
-                  );
-                },
+    return 
+
+         
+         Card(
+            color: getColor(type1),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50.0)),
+              
+            
+            child: Row(
+      children: <Widget>[
+        Image.asset(sprite),
+        TextButton(
+          
+              child: Text(
+                '$name' + '  dex: $entry' + ' type: $type1',
               ),
+              onPressed: () async {
+                await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return AboutScreen();
+                    },
+                  ),
+                );
+              },
             ),
-          ),
-        ),
-      ),
+          ],
+
+            )
+
     );
+            
+      
   }
 }
