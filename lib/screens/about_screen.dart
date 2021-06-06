d5a91c174025ad736b4a16f7c547b0d8b3638a53
AlecJDavidson <alecjdavidson@outlook.com>
2021-06-06 15:49:30 -0400

    Removed unused code

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -18,21 +18,33 @@ class _AboutScreenState extends State<AboutScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TextButton(
-        child: Icon(
-          Icons.arrow_back,
-          size: 50.0,
-        ),
-        onPressed: () async {
-          await Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) {
-                return LoadingScreen();
-              },
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                alignment: Alignment.centerLeft,
+                child: TextButton(
+                  child: Icon(
+                    Icons.arrow_back,
+                    size: 50.0,
+                  ),
+                  onPressed: () async {
+                    await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return LoadingScreen();
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
             ),
-          );
-        },
+          ],
+        ),
       ),
     );
   }
